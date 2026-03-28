69dfa9ec7deb7b19da9c35e727e1bf53f66b4e6c
anish419 <manandharanish419@gmail.com>
2026-03-28 22:07:12 +0700

    FIRST SIMPLE PARSER CREATED

--- compiler-design/main.py
+++ compiler-design/main.py
@@ -1,18 +1,24 @@
 from calclex import CalcLexer
+from calcparser import CalcParser
 
 def main():
     print("Hello from compiler-design!")
 
     lexer = CalcLexer()
+    parser = CalcParser()
 
     # data = '''x = 3 + 
     #             42 #this is a comment
     #                 * (s - t)'''
     
-    data = 'x = 3 + '
+    data = '5 - 2'
     
     for tok in lexer.tokenize(data):
         print('type=%r, value=%r' % (tok.type, tok.value))
+
+    #IMPLEMENTING PARSER
+    result = parser.parse(lexer.tokenize(data))
+    print(result)
 
 
 if __name__ == "__main__":
